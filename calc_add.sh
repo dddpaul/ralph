ebf9338a397c263546316d40628e03a2e37e14e6
Pavel Derendyaev <dddpaul@gmail.com>
2026-02-01 15:00:18 +0300

    Remove test backlog tasks and calc scripts
    Clean up artifacts from the migration test run.

--- calc_add.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# calc_add.sh - Add two numbers
-
-if [ $# -ne 2 ]; then
-  echo "Usage: calc_add.sh <num1> <num2>"
-  exit 1
-fi
-
-echo $(( $1 + $2 ))
--- calc_mul.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# calc_mul.sh - Multiply two numbers
-
-if [ $# -ne 2 ]; then
-  echo "Usage: calc_mul.sh <num1> <num2>"
-  exit 1
-fi
-
-echo $(( $1 * $2 ))
